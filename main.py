976730826d8a2695aaec0b6064fb413938db0bd2
taka-sea-sato <56947041+taka-sea-sato@users.noreply.github.com>
2019-10-24 15:44:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def handle_message(event):
         rep = "インキンマン"
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=os.environ[res.getResponse(event.message.text)]))
+        TextSendMessage(text= res.getResponse(event.message.text))
 
 
 if __name__ == "__main__":
