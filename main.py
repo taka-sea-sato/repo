3c126c04f0f10540c939b1c00b1e30094907ca48
taka-sea-sato <56947041+taka-sea-sato@users.noreply.github.com>
2019-10-24 16:55:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -35,59 +35,28 @@ def GetRes(Msg):
         dic = list(set(F.read().strip().split('\n')))
     first_words = [ e[0] for e in dic ]
     used = defaultdict(int)
+    
+    startswith = Msg.startswith
+    
+    msg = '{0:3}: わたしの番です。      '.format(i)
 
-    players = [ 'あなた', 'わたし' ]
-    shuffle(players)
-    startswith = None
-    for i in count(1):
-        if players[i % len(players)] == 'あなた':
-            if startswith == None:
-                startswith = choice(first_words)
-                startswith = jaconv.kata2hira(startswith)
-                
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='{0:3}: あなたの番です。 "{1}": '.format(i, startswith)))
-            s = jaconv.kata2hira(Msg)
-            # 一文字目の確認
-            while(not s.startswith(startswith)):
-                print('"{0}"で始まっていません。 '.format(startswith))
-                sys.stdout.write('{0:3}: あなたの番です。 "{1}": '.format(i, startswith))
-            # 既出単語かどうかの確認
-            if s in used:
-                print('"{0}"は{1}回目に{2}が使用しています。わたしの勝ちです。'.format(s, used[s], players[used[s]%len(players)]))
-                break
-            startswith = s[-1]
-            # 最後の文字が「ん」かどうかの確認
-            if startswith == 'ん':
-                print('最後に、ん がついたので私の勝ちです。')
-                break
-            used[s] = i
-        else:
-            msg = '{0:3}: わたしの番です。      '.format(i)
-            if startswith == None:
-                word = choice(dic)
-                convword = jaconv.kata2hira(word)
-                startswith = convword[-1]
-                used[convword] = i
-                
-                line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg + '{0}'.format(word)))
-            else:
-                words = [ e for e in dic if jaconv.kata2hira(e).startswith(startswith) and e not in used ]
-                if len(words) == 0:
-                    print('もう思いつきません! あなたの勝ちです。')
-                    print('{0} から始まる言葉を教えてください！'.format(startswith))
-                    s = input()
-                    # 回答リストに書き込む処理
-                    with(open(ansfilename, 'a')) as F:
-                        F.write(s)
-                        F.write('\n')
-                    print('ありがとうございます！これでまた賢くなりました！')
-                    break
-                else:
-                    word = choice(words)
-                    convword = jaconv.kata2hira(word)
-                    startswith = convword[-1]
-                    used[word] = i
-                    print(msg + '{0}'.format(word))
+    words = [ e for e in dic if jaconv.kata2hira(e).startswith(startswith) and e not in used ]
+    if len(words) == 0:
+        print('もう思いつきません! あなたの勝ちです。')
+        print('{0} から始まる言葉を教えてください！'.format(startswith))
+        s = input()
+        # 回答リストに書き込む処理
+        with(open(ansfilename, 'a')) as F:
+            F.write(s)
+            F.write('\n')
+        print('ありがとうございます！これでまた賢くなりました！')
+        break
+    else:
+        word = choice(words)
+        convword = jaconv.kata2hira(word)
+        startswith = convword[-1]
+        used[word] = i
+        print(msg + '{0}'.format(word))
     print('今回のしりとりでは{0}個の単語を使用しました。'.format(len(used)))
 
 
