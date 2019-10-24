5f43cecd1f31e7f63603acda14a8551f43b160b4
taka-sea-sato <56947041+taka-sea-sato@users.noreply.github.com>
2019-10-24 16:06:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -40,15 +40,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text == "あ":
-        rep = "アンパンマン"
-    if event.message.text == "い":
-        rep = "インキンマン"
-        
-    rep = "テスト"
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text = rep)
+        TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
