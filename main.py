a5e7f54295d9c5dc6f3fee1f2e12d2d004ec9079
taka-sea-sato <56947041+taka-sea-sato@users.noreply.github.com>
2019-10-24 17:57:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -89,6 +89,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     
+    gameflg = 1
+    
     if event.message.text == "しりとりしよ":
         gameflg = 1
         used = defaultdict(int)
